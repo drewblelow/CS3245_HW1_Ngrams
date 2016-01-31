cbda03aa8b2f8179547dac695ad692b96328c3c4
Andrew Low <andrewjrlow@gmail.com>
2016-01-31 19:32:57 +0800

    regex split

--- test.py
+++ test.py
@@ -8,6 +8,15 @@ SIZE_NGRAM = 4
 START_TOKEN = '+'
 END_TOKEN = '-'
 
+def ngram_from_line(line, ngram_size = SIZE_NGRAM):
+	"""
+	builds n grams from a string input line
+	"""
+	fourgrams = re.findall(r'(?=(...))', line)
+	for grams in fourgrams:
+		print grams
+	print "done"
+
 def test(in_file):
 	"""
 	build language models for each label
@@ -16,12 +25,17 @@ def test(in_file):
 	print 'building language models...'
 	file_contents = open(in_file).readlines()
 	"""
-	for each line in the file, read the language type
-	then for character sequences in that line, add the ngrams to the LM
+	for each line in the file, split the language type away from the text line
+	split the text line into n grams and add it to the correct language type
 	"""
 	for line in file_contents:
-		language_type = line.partition(' ')[0]
+		split_line = line.split(' ', 1)
+		language_type = split_line[0]
+		text_line = split_line[1]
 		print language_type
+		print text_line
+		break
 
 input_file_t = 'D:\\ProgramFiles(x86)\\temp\\A0110649J\\input.train.txt'
 test(input_file_t)
+ngram_from_line('hello asdfgh')
