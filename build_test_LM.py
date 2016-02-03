5ed80f72b71d43ba3d6d2524e6477639af6435fd
Andrew Low <andrewjrlow@gmail.com>
2016-02-03 11:53:38 +0800

    changed regex ngrams to iterative ngrams -can now specify size of ngram

--- build_test_LM.py
+++ build_test_LM.py
@@ -3,38 +3,61 @@
 import re
 import nltk
 import sys
+import os.path
 import getopt
 
 SIZE_NGRAM = 4
 
+# method to add padding
+def maxxi_padding(list, ngram_size = SIZE_NGRAM):
+	for counter in range (1, SIZE_NGRAM):
+		list.insert(0, START_TOKEN)
+		list.append(END_TOKEN)
+
+# method to get ngrams from the sentence 
+def ngram_from_line(line, ngram_size = SIZE_NGRAM):
+	line_list = list(line)
+	maxxi_padding(line_list)
+	# loop - process all the fourgrams in the line
+	fourgrams = []
+	for counter in range (0, len(line_list) - ngram_size + 1):
+		gram = []
+		for counter_2 in range(0, ngram_size):
+			gram.append(line_list[counter + counter_2])
+		fourgrams.append(gram)
+	return fourgrams
+
 def build_LM(in_file):
-    """
-    build language models for each label
-    each line in in_file contains a label and an URL separated by a tab(\t)
-    """
-    print 'building language models...'
-    # This is an empty method
-    # Pls implement your code in below
-    
-def test_LM(in_file, out_file, LM):
-    """
-    test the language models on new URLs
-    each line of in_file contains an URL
-    you should print the most probable label for each URL into out_file
-    """
-    print "testing language models..."
-    # This is an empty method
-    # Pls implement your code in below
+	"""
+	build language models for each label
+	each line in in_file contains a label and an URL separated by a tab(\t)
+	"""
+	print 'building language models...'
+	file_contents = open(in_file).readlines()
+	"""
+	for each line in the file, split the language type away from the text line
+	split the text line into n grams and add it to the correct language type
+	"""
+	for line in file_contents:
+		split_line = line.split(' ', 1)
+		language_type = split_line[0]
+		text_line = split_line[1]
+		line_fourgram = ngram_from_line(text_line)
+		#print text_line
+	
+	print 'finished building'
 
 def usage():
-    print "usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file"
-
-input_file_b = input_file_t = output_file = None
+	print "usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file"
+	
+input_file_b = "input.train.txt"
+input_file_t = output_file = None
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'b:t:o:')
 except getopt.GetoptError, err:
     usage()
     sys.exit(2)
+"""
 for o, a in opts:
     if o == '-b':
         input_file_b = a
@@ -44,9 +67,11 @@ for o, a in opts:
         output_file = a
     else:
         assert False, "unhandled option"
+"""
+"""
 if input_file_b == None or input_file_t == None or output_file == None:
     usage()
     sys.exit(2)
-
+"""
 LM = build_LM(input_file_b)
-test_LM(input_file_t, output_file, LM)
+#test_LM(input_file_t, output_file, LM)
--- test.py
+++ test.py
@@ -5,59 +5,65 @@ import os.path
 import getopt
 
 SIZE_NGRAM = 4
+START_TOKEN = "START_TOKEN_CHAR"
+END_TOKEN = "END_TOKEN_CHAR"
 
+# method to add padding
+def maxxi_padding(list, ngram_size = SIZE_NGRAM):
+	for counter in range (1, SIZE_NGRAM):
+		list.insert(0, START_TOKEN)
+		list.append(END_TOKEN)
+
+# method to get ngrams from the sentence 
 def ngram_from_line(line, ngram_size = SIZE_NGRAM):
-	"""
-	builds n grams from a string input line using regex
-	"""
-	fourgrams = re.findall(r'(?=(...))', line)
+	line_list = list(line)
+	maxxi_padding(line_list)
+	# loop - process all the fourgrams in the line
+	fourgrams = []
+	for counter in range (0, len(line_list) - ngram_size + 1):
+		gram = []
+		for counter_2 in range(0, ngram_size):
+			gram.append(line_list[counter + counter_2])
+		fourgrams.append(gram)
 	return fourgrams
-
-def build_LM(in_file):
-	"""
-	build language models for each label
-	each line in in_file contains a label and an URL separated by a tab(\t)
-	"""
-	print 'building language models...'
-	file_contents = open(in_file).readlines()
-	"""
-	for each line in the file, split the language type away from the text line
-	split the text line into n grams and add it to the correct language type
-	"""
-	for line in file_contents:
-		split_line = line.split(' ', 1)
-		language_type = split_line[0]
-		text_line = split_line[1]
-		line_fourgram = ngram_from_line(text_line)
-		#print text_line
 	
-	print 'finished building'
+def test_printer(thing):
+	for gram in thing:
+		print(gram)
 
-def usage():
-	print "usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file"
-	
-input_file_b = "input.train.txt"
-input_file_t = output_file = None
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'b:t:o:')
-except getopt.GetoptError, err:
-    usage()
-    sys.exit(2)
+def exit():
+	sys.exit(0)
+
+TEST_LINE = "asdfghjk lqw ertyu iop"
+process = ngram_from_line(TEST_LINE)
+test_printer(process)
+exit()
+
 """
-for o, a in opts:
-    if o == '-b':
-        input_file_b = a
-    elif o == '-t':
-        input_file_t = a
-    elif o == '-o':
-        output_file = a
-    else:
-        assert False, "unhandled option"
+# this method builds n grams from a string input line by iterating through the list
+def ngram_from_line1(line, ngram_size = SIZE_NGRAM):
+	line_list = list(line)
+	# loop - to attach start tokens to the first few ngrams
+	fourgrams = []
+	for counter in range(1, ngram_size):
+		gram = []
+		for counter_2 in range(counter, ngram_size):
+			gram.append(START_TOKEN)
+		for counter_3 in range(0, counter):
+			gram.append(line_list[counter_3])
+		fourgrams.append(gram)
+	# loop - process all the fourgrams that contain characters in the line
+	for counter in range (0, len(line_list) - ngram_size + 1):
+		gram = []
+		for counter_2 in range(0, ngram_size):
+			gram.append(line_list[counter + counter_2])
+		fourgrams.append(gram)
+	# loop - attach end tokens to the last few ngrams	
+	for counter in range(len(line_list) - ngram_size + 1, len(line_list)):
+		gram = []
+		
+		for counter_4 in range(len(line_list) - counter, ngram_size):
+			gram.append(END_TOKEN)
+		fourgrams.append(gram)
+	return fourgrams
 """
-"""
-if input_file_b == None or input_file_t == None or output_file == None:
-    usage()
-    sys.exit(2)
-"""
-LM = build_LM(input_file_b)
-#test_LM(input_file_t, output_file, LM)
