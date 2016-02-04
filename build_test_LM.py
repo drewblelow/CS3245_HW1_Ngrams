c8eee0aa3e35cccefa8ac6a1de8d2d77751c2968
Andrew Low <andrewjrlow@gmail.com>
2016-02-04 16:38:30 +0800

    +1 smoothing

--- build_test_LM.py
+++ build_test_LM.py
@@ -8,6 +8,7 @@ import getopt
 
 SIZE_NGRAM = 4
 LANGUAGE_MODEL = {}
+UNIQUE_GRAMS = []
 
 # method to add padding
 def maxxi_padding(list, ngram_size = SIZE_NGRAM):
@@ -28,15 +29,27 @@ def ngram_from_line(line, ngram_size = SIZE_NGRAM):
 		fourgrams.append(gram)
 	return fourgrams
 
+# method checks the dictionary for the language, then adds ngrams to it if it exists. Creates a key otherwise
 def dict_update(language, ngrams):
 	gram_list = list(ngrams)
 	if (not language in LANGUAGE_MODEL):
 		LANGUAGE_MODEL[language] = gram_list
+		for gram in gram_list:
+			if (not gram in UNIQUE_GRAMS):
+				UNIQUE_GRAMS.append(gram)
 	else :
 		current_list = LANGUAGE_MODEL.get(language)
 		for gram in gram_list:
 			current_list.append(gram)
+			if (not gram in UNIQUE_GRAMS):
+				UNIQUE_GRAMS.append(gram)
 		LANGUAGE_MODEL[language] = current_list
+	
+# method does smootihng for the ngrams
+def smooth_dict():
+	for key in LANGUAGE_MODEL:
+		ngram_set = LANGUAGE_MODEL.get(key)
+		ngram_set.extend(UNIQUE_GRAMS)
 	
 def build_LM(in_file):
 	"""
@@ -55,13 +68,25 @@ def build_LM(in_file):
 		text_line = split_line[1]
 		line_fourgram = ngram_from_line(text_line)
 		dict_update(language_type, line_fourgram)
-	print 'finished building'
+	smooth_dict()
+	return LANGUAGE_MODEL
 
+def test_LM(in_file, out_file, LM):
+    """
+    test the language models on new URLs
+    each line of in_file contains an URL
+    you should print the most probable label for each URL into out_file
+    """
+    print "testing language models..."
+    # This is an empty method
+    # Pls implement your code in below
+	
 def usage():
 	print "usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file"
 	
 input_file_b = "input.train.txt"
-input_file_t = output_file = None
+input_file_t = "input.test.txt"
+output_file = "output.txt"
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'b:t:o:')
 except getopt.GetoptError, err:
